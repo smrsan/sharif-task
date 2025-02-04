a76b55611efed903fd4e657ed3cc0dbc4e517eec
smrsan <57676740+smrsan@users.noreply.github.com>
2025-02-04 05:02:47 +0330

    Handle form validation in login & register forms

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,28 +1,36 @@
-import { ChangeEventHandler, useCallback, useState } from "react";
+import { useCallback } from "react";
 import { Link as RouterLink, useNavigate } from "react-router";
 import { Box, Button, Link, Paper, TextField } from "@mui/material";
 import login from "@/helpers/login";
+import * as yup from "yup";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+const loginSchema = yup.object({
+  username: yup.string().required("Username is required"),
+  password: yup
+    .string()
+    .min(6, "Password must be at least 6 characters")
+    .required("Password is required"),
+});
 
 function LoginPage() {
   const nav = useNavigate();
-  const [formValues, setFormValues] = useState({
-    username: "",
-    password: "",
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(loginSchema),
   });
 
-  const handleChange = useCallback<
-    ChangeEventHandler<HTMLTextAreaElement | HTMLInputElement>
-  >((event) => {
-    setFormValues((p) => ({
-      ...p,
-      [event.target.name]: event.target.value,
-    }));
-  }, []);
-
-  const handleLogin = useCallback(async () => {
-    login(formValues);
-    nav("/dashboard");
-  }, [formValues, nav]);
+  const handleLogin = useCallback(
+    async (formValues: any) => {
+      login(formValues);
+      nav("/dashboard");
+    },
+    [nav]
+  );
 
   return (
     <Paper
@@ -33,23 +41,25 @@ function LoginPage() {
         display: "grid",
         gap: 2,
       }}
+      component="form"
+      onSubmit={handleSubmit(handleLogin)}
     >
       <TextField
-        name="username"
-        onChange={handleChange}
-        value={formValues["username"]}
         label="Username"
         fullWidth
         autoFocus
         type="text"
+        {...register("username")}
+        error={errors.username != null}
+        helperText={errors.username?.message}
       />
       <TextField
-        name="password"
-        onChange={handleChange}
-        value={formValues["password"]}
         label="Password"
         fullWidth
         type="password"
+        {...register("password")}
+        error={errors.password != null}
+        helperText={errors.password?.message}
       />
       <Box
         sx={{
@@ -61,7 +71,7 @@ function LoginPage() {
         <Link component={RouterLink} to="/register">
           Haven't an account?
         </Link>
-        <Button onClick={handleLogin}>Login</Button>
+        <Button type="submit">Login</Button>
       </Box>
     </Paper>
   );
--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,29 +1,37 @@
-import { ChangeEventHandler, useCallback, useState } from "react";
+import { useCallback } from "react";
 import { Link as RouterLink, useNavigate } from "react-router";
 import { Box, Button, Link, Paper, TextField } from "@mui/material";
-import register from "@/helpers/register";
+import registerHelper from "@/helpers/register";
+import * as yup from "yup";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+const registerSchema = yup.object({
+  username: yup.string().required("Username is required"),
+  email: yup.string().email("Invalid email").required("Email is required"),
+  password: yup
+    .string()
+    .min(6, "Password must be at least 6 characters")
+    .required("Password is required"),
+});
 
 function RegisterPage() {
   const nav = useNavigate();
-  const [formValues, setFormValues] = useState({
-    username: "",
-    email: "",
-    password: "",
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(registerSchema),
   });
 
-  const handleChange = useCallback<
-    ChangeEventHandler<HTMLTextAreaElement | HTMLInputElement>
-  >((event) => {
-    setFormValues((p) => ({
-      ...p,
-      [event.target.name]: event.target.value,
-    }));
-  }, []);
-
-  const handleRegister = useCallback(async () => {
-    register(formValues);
-    nav("/dashboard");
-  }, [formValues, nav]);
+  const handleRegister = useCallback(
+    async (formValues: any) => {
+      registerHelper(formValues);
+      nav("/dashboard");
+    },
+    [nav]
+  );
 
   return (
     <Paper
@@ -34,32 +42,34 @@ function RegisterPage() {
         display: "grid",
         gap: 2,
       }}
+      component="form"
+      onSubmit={handleSubmit(handleRegister)}
     >
       <TextField
-        name="username"
-        onChange={handleChange}
-        value={formValues["username"]}
         label="Username"
         fullWidth
         autoFocus
         type="text"
+        {...register("username")}
+        error={errors.username != null}
+        helperText={errors.username?.message}
       />
       <TextField
-        name="email"
-        onChange={handleChange}
-        value={formValues["email"]}
         label="Email"
         fullWidth
         autoFocus
         type="email"
+        {...register("email")}
+        error={errors.email != null}
+        helperText={errors.email?.message}
       />
       <TextField
-        name="password"
-        onChange={handleChange}
-        value={formValues["password"]}
         label="Password"
         fullWidth
         type="password"
+        {...register("password")}
+        error={errors.password != null}
+        helperText={errors.password?.message}
       />
       <Box
         sx={{
@@ -71,7 +81,7 @@ function RegisterPage() {
         <Link component={RouterLink} to="/login">
           Already have an account?
         </Link>
-        <Button onClick={handleRegister}>Register</Button>
+        <Button type="submit">Register</Button>
       </Box>
     </Paper>
   );
