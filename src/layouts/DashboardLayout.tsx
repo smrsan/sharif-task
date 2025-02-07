c438350fafbc9231453e21ab19929802bc2dc644
smrsan <57676740+smrsan@users.noreply.github.com>
2025-02-07 21:39:23 +0330

    Make the whole background of DashboardLayout grey

--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -1,5 +1,5 @@
 import isLogin from "@/helpers/isLogin";
-import { Container } from "@mui/material";
+import { Box, Container } from "@mui/material";
 import { useEffect } from "react";
 import { Outlet, useNavigate } from "react-router";
 import TopBar from "@/components/TopBar";
@@ -13,16 +13,21 @@ const DashboardLayout = () => {
   }, [nav]);
 
   return (
-    <Container
-      maxWidth="md"
+    <Box
       sx={{
-        minHeight: "100vh",
         bgcolor: (theme) => theme.palette.grey[200],
       }}
     >
-      <TopBar />
-      <Outlet />
-    </Container>
+      <Container
+        maxWidth="md"
+        sx={{
+          minHeight: "100vh",
+        }}
+      >
+        <TopBar />
+        <Outlet />
+      </Container>
+    </Box>
   );
 };
 
